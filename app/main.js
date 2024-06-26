ddecb7469ea67c444812f0ecd385399bca8c12b5
HeberthDev <heberthdeveloper@gmail.com>
2024-06-26 07:52:04 -0300

    forEach de livros

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 let livros = [];
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+const elementoParaInserirLivros = document.getElementById('livros');
 
 getBuscarLivrosDaAPI()
 
@@ -8,4 +9,40 @@ async function getBuscarLivrosDaAPI() {
 
     livros = await res.json();
     console.table(livros);
+    exibirOsLivrosNaTela(livros);
+}
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+    listaDeLivros.forEach((livro) => {
+        elementoParaInserirLivros.innerHTML += `
+            <div class="livro">
+                <img 
+                    class="livro__imagens" 
+                    src="${livro.imagem}"
+                    alt="${livro.alt}" 
+                />
+
+                <h2 class="livro__titulo">
+                    ${livro.titulo}
+                </h2>
+
+                <p class="livro__descricao">
+                    ${livro.autor}
+                </p>
+
+                <p 
+                    class="livro__preco" 
+                    id="preco"
+                >   
+                    ${livro.preco}
+                </p>
+
+                <div class="tags">
+                    <span class="tag">
+                        ${livro.categoria}
+                    </span>
+                </div>
+            </div>
+        `
+    })
 }
