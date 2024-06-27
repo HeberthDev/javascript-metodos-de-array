684ab14372e8d98da94ae0570bd1d80fa2c1a8d5
HeberthDev <heberthdeveloper@gmail.com>
2024-06-27 08:26:43 -0300

    Filtrando livros disponíveis

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -6,7 +6,7 @@ function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const categoria = elementoBtn.value;
 
-    let livrosFiltrados = livros.filter((livro) =>
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter( (livro) => { return livro.quantidade > 0 }) : livros.filter((livro) =>
         livro.categoria == categoria
     );
 
