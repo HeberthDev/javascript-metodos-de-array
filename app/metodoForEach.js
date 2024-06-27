b9c108e72262084f38f33a37e60b962b3f38d2ea
HeberthDev <heberthdeveloper@gmail.com>
2024-06-27 08:17:14 -0300

    Livros indisponíveis

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -4,10 +4,13 @@ function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = ' ';
 
     listaDeLivros.forEach((livro) => {
+        // let disponibilidade = verificarDisponibilidadeDoLivro(livro);
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';
+
         elementoParaInserirLivros.innerHTML += `
             <div class="livro">
                 <img 
-                    class="livro__imagens" 
+                    class="${disponibilidade}" 
                     src="${livro.imagem}"
                     alt="${livro.alt}" 
                 />
@@ -36,3 +39,11 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         `
     })
 }
+
+// function verificarDisponibilidadeDoLivro(livro) {
+//     if (livro.quantidade > 0) {
+//         return 'livro__imagens';
+//     } else {
+//         return 'livro__imagens indisponivel';
+//     }
+// }
